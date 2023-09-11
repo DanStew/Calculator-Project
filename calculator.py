991a627cf826178ade9f177e91fa8eddd52a79d4
DanStew <danielstewarts2004@gmail.com>
2023-09-11 23:50:24 +0100

    Project Description and GUI Building

---
+++ calculator.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+calculation = ""
+
+#Function used to add a symbol to the calculation
+def add_to_calculation(symbol):
+    pass
+
+#Function used to work out the result of the calculation
+def evaluate_calculation():
+    pass
+
+#Function to reset the calculation
+def clear_field():
+    pass
+
+#Building the GUI
+root = tk.Tk()
+root.geometry("300x275")
+root.mainloop()
---
+++ readMe.md
@@ -0,0 +1,2 @@
+This is a basic calculator project made using python
+The main aim of this project is to understand how to make and manipulate GUIs using python
